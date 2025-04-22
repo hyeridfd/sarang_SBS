5239c70c07f1c175405e853d314788f86f9b0bc7
hyeridfd <160723285+hyeridfd@users.noreply.github.com>
2025-04-22 09:48:09 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -312,6 +312,8 @@ st.set_page_config(page_title="SNU CareFit +", layout="wide")
 
 st.image("./logo.png", width=300)
 
+st.image("./sarang.png", width=300)
+
 st.markdown(
     '<h3 style="color:#226f54; font-size:50px; font-weight:bold;">SNU CareFit +</h3>',
     unsafe_allow_html=True
